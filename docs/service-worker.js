78bd0baccc27bac0eb0c70f44642ad058b5f1955
Masami Sugao <15377854+masami-sugao@users.noreply.github.com>
2025-10-27 21:42:31 +0900

    :bug: ギターの本数が増えて横スクロールが発生すると一番左までスクロールできない問題を修正

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: IBZgsKK8 */
+/* Manifest version: pvmRYAEb */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
