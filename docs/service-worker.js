aaa1f409d18c9bb6482aa948ca3d57db2161883d
Masami Sugao <15377854+masami-sugao@users.noreply.github.com>
2025-10-28 19:15:57 +0900

    :rocket: 公開用ファイル生成

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: pvmRYAEb */
+/* Manifest version: 6A0N5rQb */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
